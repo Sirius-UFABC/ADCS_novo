502b7cd0d075280875095d8d71db063bed519e63
leoSartori1 <162925837+leoSartori1@users.noreply.github.com>
2024-05-28 16:53:49 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/gpio.h"
+
+#define PIN 14
+
 
 void app_main(void)
 {
+     esp_rom_gpio_pad_select_gpio(PIN);
+    gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
 
+    bool status = 0;
+
+    while(1){
+        status = !status;
+        gpio_set_level(PIN, status);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
 }
